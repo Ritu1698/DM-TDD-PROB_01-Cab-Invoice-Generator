985c47e39689d937202e437f3a8f9f9233166534
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 16:30:06 +0530

    [Rituparna]Added: Ride Class and CalculateFare Method For Multiple Rides

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -10,7 +10,15 @@ public class InvoiceGenerator {
     }
 
     public double calculateFare(double distance, int time) {
-        double totalFare=distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_MINUTE;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
         return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
