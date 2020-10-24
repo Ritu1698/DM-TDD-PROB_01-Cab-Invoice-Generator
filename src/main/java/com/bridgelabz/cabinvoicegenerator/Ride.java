1d078020dbb6f67d8827e6741dbcf505ab0e34c2
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 18:08:19 +0530

    [Rituparna]Modified: Ride Class Data Member

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -3,7 +3,7 @@ package com.bridgelabz.cabinvoicegenerator;
 public class Ride {
     public double distance;
     public int time;
-    public String riderType = "normal";
+    public String riderType;
 
     public Ride(double distance, int time, String riderType) {
         this.distance = distance;
