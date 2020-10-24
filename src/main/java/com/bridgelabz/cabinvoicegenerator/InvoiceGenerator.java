bb2e90e2435073a61ac22736f2a243af5970e924
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 16:15:13 +0530

    [Rituparna]Added: Method to Calculate Total Fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator {
+    private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+
     public static void main(String args[]) {
         System.out.println("Welcome Message");
+    }
+
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
     }
 }
