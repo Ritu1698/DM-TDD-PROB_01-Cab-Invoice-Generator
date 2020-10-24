86f049d1b01d55e15d224426598cd017d4a48f58
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 16:21:15 +0530

    [Rituparna]Added: Unit Test Case For Total Fare When Fare Less Than MinimumFare

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class InvoiceGeneratorTest {
     InvoiceGenerator invoiceGenerator = null;
+
     @Before
     public void setUp() {
         invoiceGenerator = new InvoiceGenerator();
@@ -17,5 +18,14 @@ public class InvoiceGeneratorTest {
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_whenLessThanMinimumFare_shouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5.0, totalFare, 0.00);
+
     }
 }
