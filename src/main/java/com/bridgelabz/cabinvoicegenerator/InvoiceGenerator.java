baf81d0fd099da2b921e8abed95c98776f595673
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 17:55:32 +0530

    [Rituparna]Added: If Else Logic For Rider Type in CalculateFare Method

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -2,17 +2,14 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator {
     private static final int COST_PER_MINUTE = 1;
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
     private static final double MINIMUM_FARE = 5.0;
-    private RideRepository rideRepository = null;
+    private static final int COST_PER_MINUTE_PREMIUM = 2;
+    private static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15.0;
+    private static final double MINIMUM_FARE_PREMIUM = 20.0;
 
     public static void main(String args[]) {
         System.out.println("Welcome Message");
-    }
-
-
-    public InvoiceGenerator() {
-        this.rideRepository = new RideRepository();
     }
 
     public double calculateFare(double distance, int time) {
@@ -20,10 +17,20 @@ public class InvoiceGenerator {
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
+    public double calculateFare(double distance, int time, String riderType) {
+        if(riderType == "normal"){
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MINIMUM_FARE);}
+        else {
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_MINUTE_PREMIUM;
+            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
+        }
+    }
+
     public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
-            totalFare += calculateFare(ride.distance, ride.time);
+            totalFare += calculateFare(ride.distance, ride.time, ride.riderType);
         }
         return new InvoiceSummary(rides.length, totalFare);
     }
--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -3,9 +3,15 @@ package com.bridgelabz.cabinvoicegenerator;
 public class Ride {
     public double distance;
     public int time;
+    public String riderType = "normal";
 
     public Ride(double distance, int time) {
         this.distance = distance;
         this.time = time;
+    }
+    public Ride(double distance, int time, String riderType) {
+        this.distance = distance;
+        this.time = time;
+        this.riderType = riderType;
     }
 }
