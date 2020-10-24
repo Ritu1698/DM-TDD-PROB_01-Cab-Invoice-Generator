f7a76478835d54646d2718edbd6a0ec6b811fcfc
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-24 16:12:45 +0530

    [Rituparna]Added: Unit Test Case For Total Fair

---
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = null;
+    @Before
+    public void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+}
